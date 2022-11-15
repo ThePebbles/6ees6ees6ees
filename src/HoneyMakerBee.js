e990038ceedb5198cd690e0f68ea0a40b003e727
Wisesofthemall <foxxydieujuste@gmail.com>
2022-11-14 22:12:51 -0500

    HoneyMaker Bee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,16 @@
+b = new Bee()
 class HoneyMakerBee {
+  age = 10;
+  job = 'make honey';
+  color = b.color;
+  food = g.food;
+  eat = g.eat;
+  honeyPot = 0;
+  makeHoney = function(){
+    this.honeyPot++;
+  }
+  giveHoney = function(){
+  this.honeyPot--;
+  }
 
 };
