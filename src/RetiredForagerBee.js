ed8df16803273f55e9502041dde32ae75f942ff7
Wisesofthemall <foxxydieujuste@gmail.com>
2022-11-14 22:23:53 -0500

    RetiredForager Bee

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,5 +1,19 @@
-
+var fb = new ForagerBee();
 class RetiredForagerBee {
+  age = 40;
+  job = 'gamble';
+  canFly = false;
+  color = 'grey';
+  forage = function(){
+    return "I am too old, let me play cards instead";
+  }
+  food = g.food;
+  eat = g.eat;
+  treasureChest= fb.treasureChest;
+  gamble = function(){
+    this.treasureChest.push('treasure');
+  }
+
 
 
 };
